18f28a1edf365231a4d93229985007abd7b8876f
Arthur Pruvost Rivière <96424611+igibi1791@users.noreply.github.com>
2024-11-07 22:18:39 +0100

    Ajout du prototype de comptePoint

--- mesFonctions.h
+++ mesFonctions.h
@@ -7,6 +7,7 @@ using namespace std;
 void affiche(const vector<int>&, const vector<string>&, const vector<int>&, int, const vector<int>&, const vector<int>&);
 void afficheSentier(const vector<int>&, const vector<string>&, int);
 void afficheSac(const vector<int>&, const vector<int>&, const vector<string>&, const vector<int>&);
+void comptePoint(const vector<int>& sac1, const vector<int>& sac2, const vector<int>& valeurs, int& pointsJoueur1, int& pointsJoueur2);
 int getOccurence(const vector<int>&, int);
 int sommeVecteur(const vector<int>&);
 string getTresor(int, const vector<string>&);
